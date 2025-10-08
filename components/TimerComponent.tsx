a38ca5edb515d19bf43b50f821a6c82dab203d1f
Ahmet Kerem Tanrıverdi <ahmetk.tanriverdi@gmail.com>
2025-10-08 18:11:54 +0300

    Add timer icon and text color changed

--- components/TimerComponent.tsx
+++ components/TimerComponent.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { Card } from "./ui/card";
 import { Progress } from "./ui/progress";
 import { useRouter } from "next/navigation";
+import { Timer } from "lucide-react";
 
 export default function TimerComponent() {
     const [timer, setTimer] = useState<number>(1500);
@@ -24,9 +25,9 @@ export default function TimerComponent() {
     return (
         <div>
             <Card className="p-4 mt-20 h-fit lg:mt-50">
-                <p className="text-center font-bold">Kalan Süre</p>
+                <p className="font-bold flex items-center justify-center gap-2" >Kalan Süre <Timer /></p>
                 <div className="flex flex-col">
-                    <span className={`self-end font-bold text-2xl ${timer <= 500 ? 'text-red-500' : 'text-black'}`}>{Math.floor(timer / 60)}:{String(timer % 60).padStart(2, '0')}
+                    <span className={`self-end font-bold text-2xl ${timer <= 500 ? 'text-red-500' : 'text-white'}`}>{Math.floor(timer / 60)}:{String(timer % 60).padStart(2, '0')}
                     </span>
                     <Progress value={(timer / 1500) * 100} className="h-4 rounded-full mt-4" />
                 </div>
